a1e56cb956651f79d6b035c0da84229aed475222
Krunal Savaliya <krunal@hotstar.com>
2018-06-27 19:54:18 +0530

    add firebase

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { PureComponent } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+import Home from './containers/home';
 
-class App extends Component {
+class App extends PureComponent {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <Route path='/' component={Home} />
+      </Router>
     );
   }
 }
